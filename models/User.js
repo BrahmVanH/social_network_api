15e49f87b890b5808db2a618105d27808145ebdd
Brahm V <brahmvanh@gmail.com>
2023-04-04 07:57:40 -0400

    Created User schema

---
+++ models/User.js
@@ -0,0 +1,42 @@
+const { Schema, Types } = require('mongoose');
+const Thought = require('./Thought');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trimmed: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      // ADD VALIDATION USING REGEX
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
+    id: false
+  }
+);
+
+const User = model('user', userSchema);
+
+module.exports = User;
