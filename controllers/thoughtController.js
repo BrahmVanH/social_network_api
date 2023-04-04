8296537785b735afedc74d78f142a62af039e78d
Brahm V <brahmvanh@gmail.com>
2023-04-04 17:28:08 -0400

    Created empty thoughtController funcitons

---
+++ controllers/thoughtController.js
@@ -0,0 +1,23 @@
+// All thoughts by single user? Or of all users?
+const getAllThoughts = () => {};
+
+
+// Get single thought by id
+const getSingleThought = () => {};
+
+// This should create a new thought for the DB
+const createNewThought = () => {};
+// Need a function to add thought to associated user's array of thought id's
+const addThoughtToUser = () => {};
+
+const editThoughtById = () => {};
+
+const deleteThoughtById = () => {};
+
+// Reaction functions 
+
+const createReaction = () => {};
+//Need function to add reaction to reactionId array in thoughts
+const addReactionToThought = () => {};
+
+const removeReactionById = () => {};
