742b760524f236b675eba910dc01ae515afdf200
Brahm V <brahmvanh@gmail.com>
2023-04-12 18:07:16 -0400

    Finished up the thoughtController file, but I'm noticing that I will run into some issues trying to address reactions.

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -68,22 +68,38 @@ module.exports = {
 				res.status(500).json(err);
 			});
 	},
-  createReaction(req, res) {
-    Thought.findOneAndUpdate(
-      { _id: req.params.thoughtId },
-      { $push: { reactions: { reactionBody: req.body.reactionBody, username: req.body.username } } },
-      { runValidator: true, new: true }
-    )
-    .then((thought) => 
-    !thought
-      ? res.status(404).json({ message: "No thoughts found with that Id!" })
-      : res.json(thought)
-      )
-      .catch((err) => res.status(500).json(err));
-  }
+	createReaction(req, res) {
+		Thought.findOneAndUpdate(
+			{ _id: req.params.thoughtId },
+			{
+				$push: {
+					reactions: {
+						reactionBody: req.body.reactionBody,
+						username: req.body.username,
+					},
+				},
+			},
+			{ runValidator: true, new: true }
+		)
+			.then((thought) =>
+				!thought
+					? res.status(404).json({ message: 'No thoughts found with that Id!' })
+					: res.json(thought)
+			)
+			.catch((err) => res.status(500).json(err));
+	},
+	removeReactionById(req, res) {
+		Thought.findOneAndUpdate(
+			{ id: req.params.thoughtId },
+			{ $pull: { reactions: req.params.reactionId } }
+		)
+			.then((thought) =>
+				!thought
+					? res.status(404).json({ message: 'No reaction found with that ID!' })
+					: res.json(thought)
+			)
+			.catch((err) => res.status(500).json(err));
+	},
 };
 
 // Reaction functions
-
-
-const removeReactionById = () => {};
