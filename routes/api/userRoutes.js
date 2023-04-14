8ddc52fbed50aaf22c578d60ffa8891e4cd5022c
Brahm V <brahmvanh@gmail.com>
2023-04-14 15:57:26 -0400

    Creater user routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,10 +1,16 @@
 const router = require('express').Router();
 const {
-  getAllUsers,
-  getSingleUser,
-  createUser,
-  updateUser,
-  deleteUser,
-  addFriend,
-  removeFriend,
+	getAllUsers,
+	getSingleUser,
+	createUser,
+	updateUser,
+	deleteUser,
+	addFriend,
+	removeFriend,
 } = require('../../controllers/userController');
+
+router.route('/').get(getAllUsers).post(createUser);
+
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+module.exports = router;
